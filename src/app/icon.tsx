394123473463373e89158c773bb6ac6856bc1b30
backspacevenkat <ghanta.v.subrhmanyam@gmail.com>
2025-10-05 23:49:37 -0700

    Fix logo rendering with simplified SVG design
    - Replace complex transform-based SVG with simple parallelogram shapes
    - Use viewBox 0 0 100 100 for better rendering at all sizes
    - Add dark slate background to favicons with white logo for better visibility
    - Fix logo not appearing in navigation
    - Make favicon more prominent in browser tabs

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -19,27 +19,30 @@ export default function Icon() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          backgroundColor: 'white',
+          backgroundColor: '#1e293b',
         }}
       >
         <svg
-          width="64"
-          height="64"
-          viewBox="0 0 600 600"
+          width="56"
+          height="56"
+          viewBox="0 0 100 100"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g transform="translate(0,600) scale(0.1,-0.1)" fill="#000000" stroke="none">
-            <path d="M2938 4023 c-31 -54 -97 -169 -148 -257 -50 -87 -96 -168 -102 -180
--8 -20 7 -52 112 -232 67 -115 149 -256 182 -314 34 -58 75 -130 93 -160 18
--30 76 -131 130 -225 134 -235 124 -221 140 -198 45 65 306 547 301 558 -13
-34 -642 1105 -649 1105 -2 0 -28 -44 -59 -97z"/>
-            <path d="M2305 2933 c-164 -285 -605 -1057 -605 -1059 0 -2 144 -4 320 -4
-l320 0 24 38 c13 20 85 143 159 272 74 129 204 357 289 505 85 149 160 280
-167 293 l12 22 -324 0 -323 0 -39 -67z"/>
-            <path d="M2678 2418 c5 -7 36 -60 67 -118 32 -58 79 -141 105 -185 26 -44 69
--117 95 -162 l48 -83 653 0 c360 0 654 2 654 3 0 2 -71 127 -159 278 l-159
-274 -657 3 c-527 2 -656 0 -647 -10z"/>
-          </g>
+          {/* Top right parallelogram */}
+          <path
+            d="M 50 10 L 85 25 L 75 50 L 40 35 Z"
+            fill="white"
+          />
+          {/* Bottom left parallelogram */}
+          <path
+            d="M 15 45 L 50 60 L 40 85 L 5 70 Z"
+            fill="white"
+          />
+          {/* Bottom right parallelogram */}
+          <path
+            d="M 50 60 L 85 75 L 75 100 L 40 85 Z"
+            fill="white"
+          />
         </svg>
       </div>
     ),
